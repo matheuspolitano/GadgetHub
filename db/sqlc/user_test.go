107a3bfe4a4207d4f03827c8f51b59d402c22873
Matheus Politano <matheuspolitano1@gmail.com>
2024-05-04 04:14:12 -0300

    added user test

---
+++ db/sqlc/user_test.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateUser(t *testing.T) {
+	userParms := CreateUserParams{
+		FirstName:    "Matheus",
+		LastName:     "Politano",
+		Email:        "Matheus Politano",
+		HashPassword: "2139u23jeio93eu12",
+		Phone:        "+12345678",
+		IsAdmin:      true,
+	}
+	user, err := testQuerier.CreateUser(context.TODO(), userParms)
+	require.NoError(t, err)
+	require.NotEmpty(t, err)
+
+	require.Equal(t, userParms.FirstName, user.FirstName)
+	require.Equal(t, userParms.LastName, user.LastName)
+	require.Equal(t, userParms.Email, user.Email)
+	require.Equal(t, userParms.HashPassword, user.HashPassword)
+	require.Equal(t, userParms.IsAdmin, user.IsAdmin)
+
+	require.NotZero(t, user.UserID)
+}
