095a4771235bcb7ae96297090394945e8d163015
Matheus Politano <matheuspolitano1@gmail.com>
2024-05-04 03:35:47 -0300

    add config system

---
+++ utils/config.go
@@ -0,0 +1,21 @@
+package utils
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DBSource string `mapstructure:"DB_SOURCE"`
+}
+
+// LoadConfig get the configuration from file or environment variables
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
+}
