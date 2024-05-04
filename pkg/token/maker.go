e81c2e6453886a392bfdfb4108aa5b679c2fd2ce
Matheus Politano <matheuspolitano1@gmail.com>
2024-05-04 16:30:29 -0300

    removed token package for while

--- pkg/token/maker.go
+++
@@ -1,8 +0,0 @@
-package token
-
-import "time"
-
-type TokenMaker interface {
-	CreateToken(username string, role string, duration time.Duration) (string, *Payload, error)
-	VerifyToken(token string) (*Payload, error)
-}
