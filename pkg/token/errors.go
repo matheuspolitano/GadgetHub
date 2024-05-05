5792b92b93b303413b81f0a77f0166cea54648b4
Matheus Politano <matheuspolitano1@gmail.com>
2024-05-05 05:46:30 -0300

    add Check Token

--- pkg/token/errors.go
+++ pkg/token/errors.go
@@ -7,5 +7,6 @@ import (
 
 var (
 	ErrExpiredToken       = errors.New("token has expired")
+	ErrInvalidToken       = errors.New("token is invalid")
 	ErrExpiredSizeInvalid = fmt.Errorf("error: The secret key must be at least %d characters long. Please provide a longer key", minLen)
 )
--- pkg/token/jwt_manager.go
+++ pkg/token/jwt_manager.go
@@ -20,12 +20,36 @@ func NewJWTManager(secretKey string) (*JWTManager, error) {
 	return &JWTManager{secretKey}, nil
 }
 
-func (manager *JWTManager) GenerateToken(payloadParameters PayloadParameters) (string, error) {
+func (manager *JWTManager) GenerateToken(payloadParameters PayloadParameters) (string, *Payload, error) {
 	payload, err := NewPayload(payloadParameters)
 	if err != nil {
-		return "", err
+		return "", nil, err
 	}
 	jwtToken := jwt.NewWithClaims(signMethod, payload)
 	jwtTokenStr, err := jwtToken.SignedString([]byte(manager.secretKey))
-	return jwtTokenStr, err
+	return jwtTokenStr, payload, err
+}
+
+func (manager *JWTManager) CheckToken(token string) (*Payload, error) {
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, ErrInvalidToken
+		}
+		return []byte(manager.secretKey), nil
+	}
+
+	jwtToken, err := jwt.ParseWithClaims(token, &Payload{}, keyFunc)
+
+	if err != nil {
+		return nil, ErrInvalidToken
+	}
+
+	payload, ok := jwtToken.Claims.(*Payload)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+
+	return payload, nil
+
 }
