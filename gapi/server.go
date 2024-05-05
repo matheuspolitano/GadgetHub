34cc60e35a59f9ac6ec5cafb13c851b5d8d3a4ea
Matheus Politano <matheuspolitano1@gmail.com>
2024-05-05 07:46:23 -0300

    added server

--- gapi/server.go
+++ gapi/server.go
@@ -4,14 +4,20 @@ import (
 	db "github.com/matheuspolitano/GadgetHub/pkg/db/sqlc"
 	"github.com/matheuspolitano/GadgetHub/pkg/pb"
 	"github.com/matheuspolitano/GadgetHub/pkg/tokenManager"
+	"github.com/matheuspolitano/GadgetHub/utils"
 )
 
 type Server struct {
 	pb.UnimplementedGadgetHubServer
 	store        db.Store
 	tokenManager tokenManager.Manager
+	config       utils.Config
 }
 
-func NewServer() Server {
-	return Server{}
+func NewServer(store db.Store, tokenManager tokenManager.Manager, config utils.Config) *Server {
+	return &Server{
+		store:        store,
+		tokenManager: tokenManager,
+		config:       config,
+	}
 }
