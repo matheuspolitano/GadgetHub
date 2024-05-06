1feb1a254c7239c36659952bf7bf7e79e155acb5
Matheus Politano <matheuspolitano1@gmail.com>
2024-05-06 16:49:03 -0300

    removed webhook

--- main.go
+++ main.go
@@ -5,10 +5,6 @@ import (
 	"log"
 	"net"
 
-	"fmt"
-
-	"net/http"
-
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/matheuspolitano/GadgetHub/gapi"
 	db "github.com/matheuspolitano/GadgetHub/pkg/db/sqlc"
@@ -18,40 +14,7 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
-func webhookHandler(w http.ResponseWriter, r *http.Request) {
-	// Check if the request method is POST
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	// Read the request body
-	body := make([]byte, r.ContentLength)
-	_, err := r.Body.Read(body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusInternalServerError)
-		return
-	}
-
-	// Print the webhook payload
-	fmt.Println("Webhook Payload:", string(body))
-
-	// Respond with success status
-	w.WriteHeader(http.StatusOK)
-}
-
 func main() {
-	// Register webhook handler function
-	http.HandleFunc("/webhook", webhookHandler)
-
-	// Start the server
-	fmt.Println("Server listening on port 9000...")
-	if err := http.ListenAndServe(":9000", nil); err != nil {
-		log.Fatal("Server error:", err)
-	}
-}
-
-func main1() {
 	conf, err := utils.LoadConfig(".")
 	if err != nil {
 		log.Fatal(err)
