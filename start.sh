6bdffd13e653de7f60c92a32bd3e674d7641f2c0
Matheus Politano <matheuspolitano1@gmail.com>
2024-05-14 03:44:58 -0300

    remove start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 
-
-#exit in error
 set -e
-
-source /app/app.env
-
-echo "Waiting for 10 seconds before running the database migration..."
-sleep 10
-
-echo "run db migration"
-
-/app/migrate -path /app/migration -database "$DB_SOURCE" --verbose up
 
 echo "start the app"
 exec "$@"
